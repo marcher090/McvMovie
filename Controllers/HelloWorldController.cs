32850c3970d7566557aaddeac8a74153acbbaf2b
marcher <marcher090@gmail.com>
2023-05-19 00:36:33 -0400

    clone from azure devops
    20230519

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+
+
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();                                                                              // The index /action method/ calls the controller's view method. View templates are made using Razor and have a .cshtml file
+                                                                                                    // extenstion and provide an elegant way of creating HTML output in c#
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");                                   
+    }
+    // GET: /HelloWorld/Test2/ 
+    public string Test2()
+    {
+        return "This is the Test2 action method...";
+    }
+}
+
+
+/*
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");                                   //In this example the ID value is taken in from the URL. Note that because the routing is "{controller=Home}/{action=Index}/{id?}",
+                                                                                                        //the url should be ending with "/HelloWorld/Welcome/3?name=Rick" and that the trailing ? after id makes it optional. 
+    }
+    // GET: /HelloWorld/Test2/ 
+    public string Test2()
+    {
+        return "This is the Test2 action method...";
+    }
+}
+*/
+
+
+
+
+
+/*public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");                         //Takes in the values from the URL localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4. This is because the ? in the URL routes the
+                                                                                                            //subsequent values after the to a query that then matches the name and numtimes values. Note that the caps doesn't matter. 
+    }
+    // GET: /HelloWorld/Test2/ 
+    public string Test2()
+    {
+        return "This is the Test2 action method...";
+    }
+}*/
+
+
+
+
+//public class HelloWorldController : Controller
+//{
+//    // 
+//    // GET: /HelloWorld/
+//    public string Index()
+//    {
+//        return "This is my default action...";
+//    }
+//    // 
+//    // GET: /HelloWorld/Welcome/ 
+//    public string Welcome()
+//    {
+//        return "This is the Welcome action method...";
+//    }
+//    // GET: /HelloWorld/Test2/ 
+//    public string Test2()
+//    {
+//        return "This is the Test2 action method...";
+//    }
+//}
+
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# McvMovie
+#Mvc Movie Web App Toutorial
+<br>
+This repo includes the code utilized to make a ASP .NET web app using a MVC framework. MVC is an acronym for Model-View-Controller and is a pattern in software design that is used when developing an application's user interfaces, data, and controlling logic.<br>
+<br>
+Model
+: Manages data and business logic 
+<br><br>
+View 
+: Handles layout and display 
+<br><br>
+Controller
+: Routes commands to the model and view parts 
+<br><br>
+Figure 1. MVC Diagram
+<br><br>
+![MVC Diagram](https://developer.mozilla.org/en-US/docs/Glossary/MVC/model-view-controller-light-blue.png)
