a6a062bf46d1e7d065bee131ebd8e88875aa056c
marcher <marcher090@gmail.com>
2023-06-01 18:57:45 -0400

    2023-06-01

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -6,8 +8,12 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
+    //The Display attribute specifies what to display for the name of a field (in this case "Release Date" instead of "ReleaseDate"). The DataType attribute specifies the type of the data (Date), so the time information stored in the field isn't displayed.
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    //The [Column(TypeName = "decimal(18, 2)")] data annotation is required so Entity Framework Core can correctly map Price to currency in the database.
     public decimal Price { get; set; }
 }
